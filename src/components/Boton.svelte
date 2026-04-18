92438e5cb9d82bbb421e51816fd65d9044d39191
Rohogrefe <rociohogrefe368@gmail.com>
2026-04-18 20:14:44 -0300

    Add files via upload

---
+++ src/components/Boton.svelte
@@ -0,0 +1,17 @@
+<script>
+    let mensaje = "bienvenido a mi curriculum";
+
+    function cambiarMensaje() {
+        mensaje = "gracias por visitar mi curriculum";
+    }
+</script>
+
+<div class="text-center mt-4">
+    <button
+        on:click={cambiarMensaje}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+        APRETA AQUI
+    </button>
+    <p class="mt-4 text-lg">{mensaje}</p>
+</div>
